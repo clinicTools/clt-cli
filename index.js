97b0d70fdc231f52702b2b173305f87e89459f46
Stefan Bittmann <s.bittmann@outlook.de>
2020-12-03 09:07:43 +0100

    update check

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 #! /usr/bin/env node
 let args = require("minimist")(process.argv.slice(2));
+let axios = require("axios").default
+let version = require(__dirname + "/version.json").version;
 
 (async () => {
+    console.log(version)
+
     if (args._[0] == "config") {
         await require(__dirname + "/lib/config.js")(args);
     } else if (args._[0] == "serve") {
@@ -10,6 +14,12 @@ let args = require("minimist")(process.argv.slice(2));
         await require(__dirname + "/lib/publish.js")(args);
     } else if (args._[0] == "build") {
         await require(__dirname + "/lib/build.js")(args);
+    } else if (args._[0] == "update") {
+        await require(__dirname + "/lib/update.js")(args);
     }
-    process.exit();
+    
 })();
+
+/*
+https://raw.githubusercontent.com/clinicTools/clt-cli/main/index.js
+*/
