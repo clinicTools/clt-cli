1c29dba3d6e9aa0a29175d8d1ebc1aaf39684c73
Stefan Bittmann <s.bittmann@outlook.de>
2020-12-03 12:30:30 +0100

    Update Process

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 #! /usr/bin/env node
 let args = require("minimist")(process.argv.slice(2));
 let axios = require("axios").default
+let chalk = require("chalk");
 let version = require(__dirname + "/version.json").version;
 
 (async () => {
-    console.log(version)
+    await updateCheck()
 
-    if (args._[0] == "config") {
-        await require(__dirname + "/lib/config.js")(args);
-    } else if (args._[0] == "serve") {
-        await require(__dirname + "/lib/serve.js")(args);
-    } else if (args._[0] == "publish") {
-        await require(__dirname + "/lib/publish.js")(args);
-    } else if (args._[0] == "build") {
-        await require(__dirname + "/lib/build.js")(args);
-    } else if (args._[0] == "update") {
-        await require(__dirname + "/lib/update.js")(args);
+    if (["config", "serve", "publish", "build", "update"].includes(args._[0])) {
+        await require(`${__dirname}/lib/${args._[0]}.js`).default(args);
+    } else {
+        console.error(chalk`{red Command unknown:} command`);
     }
-    
 })();
 
-/*
-https://raw.githubusercontent.com/clinicTools/clt-cli/main/index.js
-*/
+async function updateCheck() {
+    try {
+        let res = await axios.get("https://raw.githubusercontent.com/clinicTools/clt-cli/main/version.json");
+        
+        let onlineVersion = res.data.version;
+        if (version !== onlineVersion) {
+            console.log(require("boxen")(
+                chalk`Update available {dim ${version}} → {green ${onlineVersion}}` + "\n" +
+                chalk`Run {cyan clt update} to update`
+                , 
+                {
+                    padding: 1,
+                    margin: 1,
+                    align: 'center',
+                    borderColor: 'yellow',
+                    borderStyle: 'round'
+                }
+            ));
+        }
+    } catch {}
+}
